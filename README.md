e71486d89671c8e4288d3e63e22e2c804ff2cd96
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-04-18 22:08:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# theBownlingGamekata
+This is kata like excercise based on http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata to teach TDD.
---
+++ src/test/java/edu/iis/mto/bowling/BowlingTest.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.bowling;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class BowlingTest {
+
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));
+	}
+
+}
