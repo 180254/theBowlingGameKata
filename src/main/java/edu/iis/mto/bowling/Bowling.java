2111e249b2037c18cfa1cf6fe44cec68cc800b36
Adrian Pędziwiatr <180254@edu.p.lodz.pl>
2015-04-19 11:57:51 +0200

    test 3 red commented out; test 2 refactor

--- src/main/java/edu/iis/mto/bowling/Bowling.java
+++ src/main/java/edu/iis/mto/bowling/Bowling.java
@@ -2,13 +2,18 @@ package edu.iis.mto.bowling;
 
 public class Bowling {
 
-	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll] = pins;
+		currentRoll++;
 	}
 
 	public int score() {
+		int score = 0;
+		for (int i = 0; i < rolls.length; i++)
+			score += rolls[i];
 		return score;
 	}
 
